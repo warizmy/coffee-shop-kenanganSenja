db39d99ced01849f1b21a7ec7abaed3246f2b17e
AbidRizmi <abidalkhaw@gmail.com>
2023-02-16 16:40:49 +0800

    mengubah code

--- js/app.js
+++ js/app.js
@@ -10,3 +10,25 @@ document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target))
     navbarNav.classList.remove("active");
 });
+
+// Messege count //
+var myText = document.getElementById("mytext");
+var result = document.getElementById("hasil");
+var limit = 489;
+result.textContent = 0 + "/" + limit;
+
+myText.addEventListener("input" , function(){
+  var textLength = myText.value.length;
+  result.textContent = textLength + "/" + limit;
+
+  if(textLength > limit){
+    myText.style.borderColor = "#FFFF";
+    result.style.color = "#FFFF";
+
+
+  }else{
+    myText.style.borderColor = "##FF0000";
+    result.style.color = "##FF0000";
+  }
+
+});
